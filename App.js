8ce604fdec2c46b7e1ba1828e218a9e924acde8b
Abasiono Udosen <abaz@Abasionos-MacBook-Pro.local>
2020-11-08 16:38:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,180 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  FindNodeHandler,
+  Text,
+  View,
+  Dimensions,
+  Animated,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+const { width, height } = Dimensions.get("screen");
+
+const images = {
+  Woman:
+    "https://images.pexels.com/photos/3772771/pexels-photo-3772771.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  kid:
+    "https://images.pexels.com/photos/4448168/pexels-photo-4448168.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  dude:
+    "https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  food:
+    "https://images.pexels.com/photos/2566040/pexels-photo-2566040.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  tech:
+    "https://images.pexels.com/photos/2007647/pexels-photo-2007647.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+};
+
+const data = Object.keys(images).map((i) => ({
+  key: i,
+  title: i,
+  image: images[i],
+  ref: React.createRef(),
+}));
+
+const Tab = React.forwardRef(({ item, onItemPress }, ref) => {
+  return (
+    <TouchableOpacity onPress={onItemPress}>
+      <View ref={ref}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 84 / data.length,
+            fontWeight: "800",
+            textTransform: "uppercase",
+          }}
+        >
+          {item.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+});
+
+const Indicator = ({ measures, scrollX }) => {
+  const inputRange = data.map((_, i) => i * width);
+  const indicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.width),
+  });
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.x),
+  });
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        height: 4,
+        width: indicatorWidth,
+        left: 0,
+        backgroundColor: "white",
+        bottom: -10,
+        transform: [
+          {
+            translateX,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+const Tabs = ({ data, scrollX, onItemPress }) => {
+  const [measures, setMeasure] = React.useState([]);
+  const containerRef = React.useRef();
+  React.useEffect(() => {
+    let m = [];
+    data.forEach((item) => {
+      item.ref.current.measureLayout(
+        containerRef.current,
+        (x, y, width, height) => {
+          m.push({
+            x,
+            y,
+            width,
+            height,
+          });
+          if (m.length === data.length) {
+            setMeasure(m);
+          }
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <View style={{ position: "absolute", top: 100, width }}>
+      <View
+        ref={containerRef}
+        style={{
+          justifyContent: "space-evenly",
+          flex: 1,
+          flexDirection: "row",
+        }}
+      >
+        {data.map((item, index) => {
+          return (
+            <Tab
+              key={item.key}
+              item={item}
+              ref={item.ref}
+              onItemPress={() => onItemPress(index)}
+            />
+          );
+        })}
+      </View>
+      {measures.length > 0 && (
+        <Indicator measures={measures} scrollX={scrollX} />
+      )}
+    </View>
+  );
+};
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const ref = React.useRef();
+  const onItemPress = React.useCallback((itemIndex) => {
+    ref?.current?.scrollToOffset({
+      offset: itemIndex * width,
+    });
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Animated.FlatList
+        data={data}
+        ref={ref}
+        keyExtractor={(item) => item.key}
+        horizontal
+        pagingEnabled
+        bounces={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        showHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, height }}>
+              <Image
+                source={{ uri: item.image }}
+                style={{ flex: 1, resizeMode: "cover" }}
+              />
+              <View
+                style={[
+                  StyleSheet.absoluteFIllObject,
+                  { backgroundColor: "rgba(0,0,0,0.3" },
+                ]}
+              />
+            </View>
+          );
+        }}
+      />
+      <Tabs scrollX={scrollX} data={data} onItemPress={onItemPress} />
     </View>
   );
 }
@@ -14,8 +182,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
